46ab58487b6ec3023f6da9cea1b8469b117c6e13
Thor77 <thor77@thor77.org>
2019-11-26 12:49:53 +0100

    Use logrus for logging

--- go.mod
+++ go.mod
@@ -5,4 +5,5 @@ go 1.13
 require (
 	github.com/grafov/m3u8 v0.11.1
 	github.com/nareix/joy4 v0.0.0-20181022032202-3ddbc8f9d431
+	github.com/sirupsen/logrus v1.4.2
 )
--- main.go
+++ main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"fmt"
 	"io"
-	"log"
 	"os"
 	"path/filepath"
 	"strings"
@@ -13,6 +12,8 @@ import (
 	"github.com/nareix/joy4/av"
 	"github.com/nareix/joy4/format/rtmp"
 	"github.com/nareix/joy4/format/ts"
+
+	log "github.com/sirupsen/logrus"
 )
 
 const addr = ":1935"
@@ -21,7 +22,7 @@ const msPerSegment = 15000
 
 // TODO: replace failln with println / switch to logrus to include stream name in msg
 
-func removeOutdatedSegments(streamName string, playlist *m3u8.MediaPlaylist) error {
+func removeOutdatedSegments(streamLogger *log.Entry, streamName string, playlist *m3u8.MediaPlaylist) error {
 	currentSegments := make(map[string]struct{}, len(playlist.Segments))
 	for _, segment := range playlist.Segments {
 		if segment != nil {
@@ -34,7 +35,11 @@ func removeOutdatedSegments(streamName string, playlist *m3u8.MediaPlaylist) err
 	}
 	for _, segmentFile := range segmentFiles {
 		if _, ok := currentSegments[segmentFile]; !ok {
-			os.Remove(segmentFile)
+			if err := os.Remove(segmentFile); err != nil {
+				streamLogger.Errorln(err)
+			} else {
+				streamLogger.Infof("Removed segment %s\n", segmentFile)
+			}
 		}
 	}
 	return nil
@@ -44,27 +49,32 @@ func main() {
 	server := &rtmp.Server{Addr: addr}
 
 	server.HandlePublish = func(conn *rtmp.Conn) {
-		log.Printf("Handling request %s", conn.URL.RequestURI())
+		log.Infof("Handling request %s\n", conn.URL.RequestURI())
 		if conn.URL.Query().Get("key") != key {
-			log.Println("Key mismatch, aborting request")
+			log.Infoln("Key mismatch, aborting request")
 			return
 		}
 
 		streamName := strings.ReplaceAll(conn.URL.Path, "/", "")
 		if streamName == "" {
-			log.Println("Invalid stream name")
+			log.Errorln("Invalid stream name")
 			return
 		}
 
+		streamLogger := log.WithFields(log.Fields{"stream": streamName})
+
 		// create hls playlist
+		playlistFileName := fmt.Sprintf("%s.m3u8", streamName)
 		playlist, err := m3u8.NewMediaPlaylist(5, 10)
 		if err != nil {
-			log.Fatal(err)
+			streamLogger.Errorln(err)
+			return
 		}
 
 		streams, err := conn.Streams()
 		if err != nil {
-			log.Fatalln(err)
+			streamLogger.Errorln(err)
+			return
 		}
 
 		i := 0
@@ -75,13 +85,15 @@ func main() {
 			segmentName := fmt.Sprintf("%s%04d.ts", streamName, i)
 			outFile, err := os.Create(segmentName)
 			if err != nil {
-				log.Fatalln(err)
+				streamLogger.Errorln(err)
+				return
 			}
 			tsMuxer := ts.NewMuxer(outFile)
 
 			// write header
 			if err := tsMuxer.WriteHeader(streams); err != nil {
-				log.Fatalln(err)
+				streamLogger.Errorln(err)
+				return
 			}
 			// write some data
 			var segmentLength time.Duration = 0
@@ -91,14 +103,16 @@ func main() {
 				var packet av.Packet
 				if packet, err = conn.ReadPacket(); err != nil {
 					if err == io.EOF {
-						log.Println("Client disconnected")
+						streamLogger.Infoln("Client disconnected")
 						clientConnected = false
 						break
 					}
-					log.Fatalln(err)
+					streamLogger.Errorln(err)
+					return
 				}
 				if err = tsMuxer.WritePacket(packet); err != nil {
-					log.Fatalln(err)
+					streamLogger.Errorln(err)
+					return
 				}
 				packetLength = packet.Time - lastPacketTime
 				segmentLength += packetLength
@@ -106,35 +120,44 @@ func main() {
 			}
 			// write trailer
 			if err := tsMuxer.WriteTrailer(); err != nil {
-				log.Fatalln(err)
+				streamLogger.Errorln(err)
+				return
 			}
-			log.Printf("Successfully wrote segment %s\n", segmentName)
+			streamLogger.Infof("Wrote segment %s\n", segmentName)
 
 			// update playlist
 			playlist.Slide(segmentName, segmentLength.Seconds(), "")
-			playlistFile, err := os.Create(fmt.Sprintf("%s.m3u8", streamName))
+			playlistFile, err := os.Create(playlistFileName)
 			if err != nil {
-				log.Fatalln(err)
+				streamLogger.Errorln(err)
+				return
 			}
 			playlistFile.Write(playlist.Encode().Bytes())
 			playlistFile.Close()
 
 			// cleanup segments
-			if err := removeOutdatedSegments(streamName, playlist); err != nil {
-				log.Println(err)
+			if err := removeOutdatedSegments(streamLogger, streamName, playlist); err != nil {
+				streamLogger.Errorln(err)
+				return
 			}
 
 			// increase counter
 			i++
 		}
 
-		// remove all segments
+		// remove all segments; this is probably not a good idea
 		for _, segment := range playlist.Segments {
 			if segment != nil {
 				if err := os.Remove(segment.URI); err != nil {
-					log.Println(err)
+					streamLogger.Errorln(err)
+					return
 				}
 			}
+		}
+		// remove playlist
+		if err := os.Remove(playlistFileName); err != nil {
+			streamLogger.Error(err)
+			return
 		}
 	}
 
